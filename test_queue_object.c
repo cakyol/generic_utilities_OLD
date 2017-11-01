aac9f0d0d379353f6d7a36daea0f4f57e859c674
Cihangir M Akyol <gene.akyol@gigamon.com>
2017-11-01 11:11:34 -0700

    still mucking about with the q object

--- test_queue_object.c
+++ test_queue_object.c
@@ -1,11 +1,9 @@
 
 #include "queue_object.h"
 
-#define QUEUE_SIZE	                1000
+#define QUEUE_SIZE	                750000
 #define QUEUE_EXPANSION_INCREMENT       128
-#define ITER_COUNT                      (QUEUE_SIZE * 100)
-
-
+#define ITER_COUNT                      (QUEUE_SIZE - 5)
 
 int main (int argc, char *argv[])
 {
@@ -13,6 +11,7 @@ int main (int argc, char *argv[])
     int i, j, n_stored;
     int64 bytes;
     double mbytes;
+    timer_obj_t timr;
 
     if (queue_obj_init(&qobj, true, QUEUE_SIZE, QUEUE_EXPANSION_INCREMENT, NULL)) {
 	fprintf(stderr, "queue_obj_init failed\n");
@@ -20,17 +19,25 @@ int main (int argc, char *argv[])
     }
 
     /* fill up the fifo */
+    start_timer(&timr);
+    printf("Populating the queue\n");
+    fflush(stdout);
     for (i = 0; i < ITER_COUNT; i++) {
         if (FAILED(queue_obj_queue_integer(&qobj, i))) {
             fprintf(stderr, "queueing %d failed\n", i);
             return -1;
         }
     }
+    end_timer(&timr);
+    report_timer(&timr, ITER_COUNT);
+
+    n_stored = qobj.n;
     OBJECT_MEMORY_USAGE(&qobj, bytes, mbytes);
 
     /* now read back and verify */
     i = 0;
-    n_stored = qobj.n;
+    printf("Now dequeuing & verifying\n");
+    start_timer(&timr);
     while (SUCCEEDED(queue_obj_dequeue_integer(&qobj, &j))) {
         if (j == i) {
             // printf("queue data %d %d verified\n", i, j);
@@ -40,9 +47,11 @@ int main (int argc, char *argv[])
         }
         i++;
     }
+    end_timer(&timr);
+    report_timer(&timr, i);
     assert((qobj.n == 0) && (i == n_stored));
-    printf("queue object is sane, capacity %d, expanded %d times, "
-            "memory %lld bytes %f mbytes\n",
+    printf("\nqueue object is sane\n  capacity %d\n  expanded %d times\n"
+            "  memory %lld bytes %f mbytes\n",
         qobj.maximum_size, qobj.expansion_count, bytes, mbytes);
     return OK;
 }
