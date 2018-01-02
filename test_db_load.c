0b4a30983c39d69e97715f08786cf45c54c2e4f7
Cihangir M Akyol <gene.akyol@gigamon.com>
2018-01-02 15:34:42 -0800

    changed test db prog to output the size of the database

--- test_db_load.c
+++ test_db_load.c
@@ -6,6 +6,8 @@ object_database_t db;
 int main (int argc, char *argv[])
 {
     int rv;
+    long long int bsize;
+    double dsize;
 
     printf("loading database .. ");
     fflush(stdout);
@@ -17,6 +19,8 @@ int main (int argc, char *argv[])
     } else {
         fprintf(stderr, "FAILED\n");
     }
+    OBJECT_MEMORY_USAGE(&db, bsize, dsize);
+    printf("db size id %lld bytes, %f megabytes\n", bsize, dsize);
     return rv;
 }
 
