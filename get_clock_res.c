4c03d01a919353ea0e65ed013903cc29274be0e6
Cihangir Akyol <gee_akyol@yahoo.com>
2018-02-11 10:37:53 -0800

    playing around with time functions

--- get_clock_res.c
+++ get_clock_res.c
@@ -10,8 +10,12 @@ int main (int argc, char *argv[])
     clock_gettime(CLOCK_MONOTONIC, &first);
     while (1) {
 	clock_gettime(CLOCK_MONOTONIC, &later);
+	sleep(1);
+#if 0
 	if (later.tv_sec != first.tv_sec) break;
 	if (later.tv_nsec != first.tv_nsec) break;
+#endif
+	printf("sec %d nsec %lld\n", later.tv_sec, later.tv_nsec);
     }
     f = first.tv_sec + first.tv_nsec * 1000000000LL;
     l = later.tvsec + later.tv_nsec * 1000000000LL;
