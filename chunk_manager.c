b106301d6431a45ba97b17996354b4f2adb02710
Cihangir M Akyol <gene.akyol@gigamon.com>
2021-10-26 22:18:31 -0700

    complete rework of chunk manager, not yet tsted however

--- chunk_manager.c
+++ chunk_manager.c
@@ -5,18 +5,27 @@
 *******************************************************************************
 *******************************************************************************
 **
-** Author: Cihangir Metin Akyol (gee.akyol@gmail.com, gee_akyol@yahoo.com)
-** Copyright: Cihangir Metin Akyol, March 2016
+** Author: Cihangir Metin Akyol, gee.akyol@gmail.com, gee_akyol@yahoo.com
+** Copyright: Cihangir Metin Akyol, April 2014 -> ....
 **
-** This code is developed by and belongs to Cihangir Metin Akyol.  
-** It is NOT owned by any company or consortium.  It is the sole
-** property and work of one individual.
+** All this code has been personally developed by and belongs to 
+** Mr. Cihangir Metin Akyol.  It has been developed in his own 
+** personal time using his own personal resources.  Therefore,
+** it is NOT owned by any establishment, group, company or 
+** consortium.  It is the sole property and work of the named
+** individual.
 **
-** It can be used by ANYONE or ANY company for ANY purpose as long 
-** as NO ownership and/or patent claims are made to it by such persons 
-** or companies.
+** It CAN be used by ANYONE or ANY company for ANY purpose as long 
+** as ownership and/or patent claims are NOT made to it by ANYONE
+** or ANY ENTITY.
 **
-** It ALWAYS is and WILL remain the property of Cihangir Metin Akyol.
+** It ALWAYS is and WILL remain the sole property of Cihangir Metin Akyol.
+**
+** For proper indentation/viewing, regardless of which editor is being used,
+** no tabs are used, ONLY spaces are used and the width of lines never
+** exceed 80 characters.  This way, every text editor/terminal should
+** display the code properly.  If modifying, please stick to this
+** convention.
 **
 *******************************************************************************
 *******************************************************************************
@@ -30,237 +39,194 @@
 extern "C" {
 #endif
 
-/*
- * adds a chunk (chp) to the end of the list.
- */
-static inline void
-cmgr_append_chunk_to_list (chunk_list_t *list, chunk_header_t *chp)
+static int
+chunk_manager_add_group_failed (chunk_manager_t *cmgrp)
 {
-    chp->next = 0;
-    if (list->n <= 0) {
-        chp->prev = 0;
-        list->head = list->tail = chp;
-    } else {
-        chp->prev = list->tail;
-        list->tail->next = chp;
-        list->tail = chp;
+    chunk_group_t *cgp;
+    byte *bp;
+    chunk_header_t *chp;
+    int i;
+
+    /* allocate chunk group structure itself */
+    cgp = MEM_MONITOR_ALLOC(cmgrp, sizeof(chunk_group_t));
+    if (NULL == cgp) return ENOMEM;
+
+    /* allocate the big chunk block */
+    cgp->chunks_block = MEM_MONITOR_ALLOC(cmgrp,
+            (cmgrp->actual_chunk_size * cmgrp->chunks_per_group));
+    if (NULL == cgp->chunks_block) {
+        MEM_MONITOR_FREE(cgp);
+        return ENOMEM;
     }
-    list->n++;
+
+    /* now expand the free chunks stack on the main chunk manager */
+    cmgrp->free_chunks_stack = MEM_MONITOR_REALLOC(cmgrp,
+	cmgrp->free_chunks_stack, cmgrp->n_cmgr_free + cmgrp->chunks_per_group);
+    if (NULL == cmgrp->free_chunks_stack) {
+        MEM_MONITOR_FREE(cgp->chunks_block);
+        MEM_MONITOR_FREE(cgp);
+        return ENOMEM;
+    }
+
+    /* All the needed memory is allocated, now update everything else */
+
+    cgp->my_manager = cmgrp;
+    cgp->n_grp_free = cmgrp->chunks_per_group;
+
+    cmgrp->n_cmgr_free += cmgrp->chunks_per_group;
+    cmgrp->n_groups += 1;
+
+    /* add all the new chunk addresses to the stack of free chunks */
+    bp = cgp->chunks_block;
+    for (i = 0; i < cmgrp->chunks_per_group; i++) {
+        chp = (chunk_header_t*) bp;
+        chp->my_group = cgp;
+        cmgrp->free_chunks_stack[i + cmgrp->free_chunks_stack_index] = chp;
+        bp += cmgrp->actual_chunk_size;
+    }
+
+    /*
+     * Everything in the group is now initialised, now
+     * add the new group to the head of groups list
+     */
+    cgp->next = cmgrp->groups;
+    cmgrp->groups = cgp;
+
+    return 0;
 }
 
 /*
- * removes the chunk (chp) out of the list.
+ * Grab a chunk from the head of the free chunks list,
+ * and return it to the caller, adjusting counters & head.
  */
-static inline void
-cmgr_remove_chunk_from_list (chunk_list_t *list, chunk_header_t *chp)
-{
-    if (chp->next == 0) {
-        if (chp->prev == 0) {
-            list->head = list->tail = 0;
-        } else {
-            chp->prev->next = 0;
-            list->tail = chp->prev;
-        }
-    } else {
-        if (chp->prev == 0) {
-            list->head = chp->next;
-            chp->next->prev = 0;
-        } else {
-            chp->prev->next = chp->next;
-            chp->next->prev = chp->prev;
-        }
-    }
-    list->n--;
-}
-
-/*
- * creates a new fresh chunk & adds it to the end of the free list
- */
-static inline int
-cmgr_add_new_chunk (chunk_manager_t *cmgr)
+static inline void *
+thread_unsafe_chunk_manager_alloc (chunk_manager_t *cmgrp)
 {
     chunk_header_t *chp;
 
-    chp = MEM_MONITOR_ALLOC(cmgr, (cmgr->chunk_size + sizeof(chunk_header_t)));
-    if (0 == chp) return ENOMEM;
-    cmgr_append_chunk_to_list(&cmgr->free_chunks, chp);
-    return 0;
-}
-
-static void
-cmgr_destroy_list (chunk_manager_t *cmgr, chunk_list_t *list)
-{
-    chunk_header_t *chp, *nxt;
-
-    chp = list->head;
-    while (chp) {
-        nxt = chp->next;
-        cmgr_remove_chunk_from_list(list, chp);
-        MEM_MONITOR_FREE(chp);
-        chp = nxt;
-    }
-    assert(list->head == 0);
-    assert(list->tail == 0);
-    assert(list->n == 0);
-}
-
-/********************* public functions *********************/
-
-/*
- * Initializes the chunk manager to specified parameters.
- * Returns 0 on success or non zero upon error.
- * How many actual chunks have been initialised is returned
- * in 'actual_chunks_created'.
- */
-int
-chunk_manager_init (chunk_manager_t *cmgr,
-        int make_it_thread_safe,
-        int chunk_size, int expansion,
-        int initial_size, int *actual_chunks_created,
-        mem_monitor_t *parent_mem_monitor)
-{
-    int failed = 0;
-
-    MEM_MONITOR_SETUP(cmgr);
-    LOCK_SETUP(cmgr);
-    safe_pointer_set(actual_chunks_created, 0);
-    cmgr->chunk_size = chunk_size;
-    cmgr->expansion = expansion;
-    cmgr->should_not_be_modified = 0;
-    cmgr->free_chunks.head = cmgr->free_chunks.tail = 0;
-    cmgr->used_chunks.head = cmgr->used_chunks.tail = 0;
-    while (initial_size-- > 0) {
-        if ((failed = cmgr_add_new_chunk(cmgr))) break;
-    }
-    safe_pointer_set(actual_chunks_created, cmgr->free_chunks.n);
-    OBJ_WRITE_UNLOCK(cmgr);
-    return failed;
-}
-
-void *
-chunk_manager_alloc (chunk_manager_t *cmgr) 
-{
-    chunk_header_t *chp;
-    int i, added;
-
-    if (cmgr->should_not_be_modified)
-        return 0;
-
-    OBJ_WRITE_LOCK(cmgr);
-
-try_again:
-
-    if (cmgr->free_chunks.n > 0) {
-        chp = cmgr->free_chunks.head;
-
-        /* move the chunk out of free list, insert into used list */
-        cmgr_remove_chunk_from_list(&cmgr->free_chunks, chp);    
-        cmgr_append_chunk_to_list(&cmgr->used_chunks, chp);
-
-        /* done */
-        OBJ_WRITE_UNLOCK(cmgr);
-        return &chp->data[0];
+    /* If there are any free chunks available on the stack, pop one */
+    if (cmgrp->n_cmgr_free > 0) {
+        assert(cmgrp->free_chunks_stack_index < cmgrp->n_cmgr_free);
+        chp = cmgrp->free_chunks_stack[cmgrp->free_chunks_stack_index];
+        (cmgrp->free_chunks_stack_index)++;
+        (chp->my_group->n_grp_free)--;
+        (cmgrp->n_cmgr_free)--;
+        return &(chp->data[0]);
     }
 
-    /* no more free chunks, try and add some more to the pool */
-    added = 0;
-    for (i = 0; i < cmgr->expansion; i++) {
-        if (cmgr_add_new_chunk(cmgr)) break;
-        added++;
-    }
-    if (added) goto try_again;
-
-    /* could not expand */
-    OBJ_WRITE_UNLOCK(cmgr);
-    return 0;
-}
-
-int
-chunk_manager_free (chunk_manager_t *cmgr, void *data)
-{
-    chunk_header_t *chp;
-
-    if (cmgr->should_not_be_modified)
-        return EBUSY;
-
-    OBJ_WRITE_LOCK(cmgr);
-    chp = (chunk_header_t*)(((char*) data) - sizeof(chunk_header_t));
-
-    /* move it out of used list into free list */
-    cmgr_remove_chunk_from_list(&cmgr->used_chunks, chp);
-    cmgr_append_chunk_to_list(&cmgr->free_chunks, chp);
-
-    /* done */
-    OBJ_WRITE_UNLOCK(cmgr);
-
-    return 0;
-}
-
-void
-chunk_manager_iterate_stop (chunk_manager_t *cmgr)
-{
-    cmgr->should_not_be_modified = 0;
-    OBJ_WRITE_UNLOCK(cmgr);
-}
-
-void *
-chunk_manager_iterate_start (chunk_manager_t *cmgr)
-{
-    OBJ_WRITE_LOCK(cmgr);
-    cmgr->should_not_be_modified = 1;
-    cmgr->iterator = cmgr->used_chunks.head;
-    if (cmgr->iterator) {
-        return &cmgr->iterator->data[0];
+    /*
+     * if we are here, no more free chunks left, so create a new
+     * group and recursively call the function again.
+     */
+    if (chunk_manager_add_group_failed(cmgrp)) {
+        return null;
     }
 
-    /* end of list */
-    chunk_manager_iterate_stop(cmgr);
-    return 0;
-}
-
-void *
-chunk_manager_iterate_next (chunk_manager_t *cmgr)
-{
-    if (cmgr->iterator) {
-        cmgr->iterator = cmgr->iterator->next;
-    }
-    if (cmgr->iterator) {
-        return &cmgr->iterator->data[0];
-    }
-
-    /* end of list */
-    chunk_manager_iterate_stop(cmgr);
-    return 0;
+    /* new group created, recursive call will return successfully */
+    return
+        thread_unsafe_chunk_manager_alloc(cmgrp);
 }
 
 /*
- * Try & return all unused chunks back to the system.
+ * Returns how many chunks were returned back to the os
+ * This must be a multiple of chunks_per_group since when
+ * chunks are returned back to the OS, entire groups are
+ * freed up.
  */
-void
-chunk_manager_trim (chunk_manager_t *cmgr)
+static int
+thread_unsafe_chunk_manager_trim (chunk_manager_t *cmgrp)
 {
-    OBJ_WRITE_LOCK(cmgr);
-    cmgr_destroy_list(cmgr, &cmgr->free_chunks);
-    assert(cmgr->free_chunks.n == 0);
-    OBJ_WRITE_UNLOCK(cmgr);
+    return 0;
 }
 
-void
-chunk_manager_destroy (chunk_manager_t *cmgr)
+/***************************** 80 column separator ****************************/
+
+PUBLIC int
+chunk_manager_init (chunk_manager_t *cmgrp,
+    boolean make_it_thread_safe,
+    int chunk_size, int chunks_per_group,
+    mem_monitor_t *parent_mem_monitor)
 {
-    OBJ_WRITE_LOCK(cmgr);
-    cmgr_destroy_list(cmgr, &cmgr->free_chunks);
-    cmgr_destroy_list(cmgr, &cmgr->used_chunks);
-    OBJ_WRITE_UNLOCK(cmgr);
-    LOCK_OBJ_DESTROY(cmgr);
-    memset(cmgr, 0, sizeof(chunk_manager_t));
+    /* basic sanity checks */
+    if ((chunk_size < MIN_CHUNK_SIZE) ||
+        (chunk_size > MAX_CHUNK_SIZE)) {
+            return EINVAL;
+    }
+    if ((chunks_per_group < MIN_CHUNKS_PER_GROUP) ||
+        (chunks_per_group > MAX_CHUNKS_PER_GROUP)) {
+            return EINVAL;
+    }
+
+    /* clear absolutely everything */
+    memset(cmgrp, 0, sizeof(chunk_manager_t));
+
+    MEM_MONITOR_SETUP(cmgrp);
+    LOCK_SETUP(cmgrp);
+
+    /* align the size to the next 8 bytes */
+    cmgrp->chunk_size = chunk_size;
+    cmgrp->actual_chunk_size =
+        ((chunk_size + 7) & ~7) + sizeof(chunk_header_t);
+
+    cmgrp->chunks_per_group = chunks_per_group;
+
+    OBJ_WRITE_UNLOCK(cmgrp);
+
+    return 0;
+}
+
+PUBLIC void *
+chunk_manager_alloc (chunk_manager_t *cmgrp)
+{
+    void *ptr;
+
+    OBJ_WRITE_LOCK(cmgrp);
+    ptr = thread_unsafe_chunk_manager_alloc(cmgrp);
+    OBJ_WRITE_UNLOCK(cmgrp);
+
+    return ptr;
+}
+
+PUBLIC void
+chunk_manager_free (void *chunk)
+{
+    chunk_header_t *chp;
+    chunk_manager_t *cmgrp;
+
+    /* get the hidden chunk header and the chunk manager pointer */
+    chp = (chunk_header_t*) (((byte*) chunk) - sizeof(chunk_header_t));
+    cmgrp = chp->my_group->my_manager;
+
+    OBJ_WRITE_LOCK(cmgrp);
+
+    /* group is being returned a chunk */
+    (chp->my_group->n_grp_free)++;
+
+    /* chunk manager is being returned one */
+    (cmgrp->n_cmgr_free)++;
+
+    /* place it into the free chunks stack */
+    (cmgrp->free_chunks_stack_index)--;
+    cmgrp->free_chunks_stack[cmgrp->free_chunks_stack_index] = chp;
+
+    OBJ_WRITE_UNLOCK(cmgrp);
+}
+
+PUBLIC int
+chunk_manager_trim (chunk_manager_t *cmgrp)
+{
+    int chunks_returned;
+
+    OBJ_WRITE_LOCK(cmgrp);
+    chunks_returned = thread_unsafe_chunk_manager_trim(cmgrp);
+    OBJ_WRITE_UNLOCK(cmgrp);
+
+    return chunks_returned;
 }
 
 #ifdef __cplusplus
 } // extern C
 #endif
-
-
 
 
 
--- chunk_manager.h
+++ chunk_manager.h
@@ -5,36 +5,27 @@
 *******************************************************************************
 *******************************************************************************
 **
-** Author: Cihangir Metin Akyol (gee.akyol@gmail.com, gee_akyol@yahoo.com)
-** Copyright: Cihangir Metin Akyol, March 2016
+** Author: Cihangir Metin Akyol, gee.akyol@gmail.com, gee_akyol@yahoo.com
+** Copyright: Cihangir Metin Akyol, April 2014 -> ....
 **
-** This code is developed by and belongs to Cihangir Metin Akyol.  
-** It is NOT owned by any company or consortium.  It is the sole
-** property and work of one individual.
+** All this code has been personally developed by and belongs to 
+** Mr. Cihangir Metin Akyol.  It has been developed in his own 
+** personal time using his own personal resources.  Therefore,
+** it is NOT owned by any establishment, group, company or 
+** consortium.  It is the sole property and work of the named
+** individual.
 **
-** It can be used by ANYONE or ANY company for ANY purpose as long 
-** as NO ownership and/or patent claims are made to it by such persons 
-** or companies.
+** It CAN be used by ANYONE or ANY company for ANY purpose as long 
+** as ownership and/or patent claims are NOT made to it by ANYONE
+** or ANY ENTITY.
 **
-** It ALWAYS is and WILL remain the property of Cihangir Metin Akyol.
+** It ALWAYS is and WILL remain the sole property of Cihangir Metin Akyol.
 **
-*******************************************************************************
-*******************************************************************************
-*******************************************************************************
-*******************************************************************************
-******************************************************************************/
-
-/******************************************************************************
-*******************************************************************************
-*******************************************************************************
-*******************************************************************************
-*******************************************************************************
-**
-** EXTREMELY fast memory allocator de-allocator for many objects of the
-** same size, called chunks.  All the chunks are pre-allocated and after
-** that retrieved & returned to the pool extremely quickly.
-**
-** On average, it is about 20 times faster from malloc/free.
+** For proper indentation/viewing, regardless of which editor is being used,
+** no tabs are used, ONLY spaces are used and the width of lines never
+** exceed 80 characters.  This way, every text editor/terminal should
+** display the code properly.  If modifying, please stick to this
+** convention.
 **
 *******************************************************************************
 *******************************************************************************
@@ -53,76 +44,149 @@ extern "C" {
 #include "mem_monitor_object.h"
 #include "lock_object.h"
 
+/******************************************************************************
+ *
+ * This is a chunk manager which manages a many same sized chunks, 
+ * dynamically allocated & freed.  It is like malloc & free but much
+ * faster since most chunks are pre-allocated and disbursing them
+ * is therefore quite fast.
+ *
+ * There is one VERY important requirement of this implementation.
+ * It is the fact that when this dynamically grows, the addresses of
+ * all previously allocated chunks MUST stay the same.  Therefore,
+ * a simple realloc scheme could not be used since at some point,
+ * a realloc may change the adresses completely, violating this
+ * principle.  The reason this is required is that if the user
+ * caches the pointer values of these chunks, they must never change.
+ *
+ * To be able to provide the above, the concept of a 'chunk group' is
+ * defined where all the addresses in that group always stay the same.
+ * In each group, a big block of malloced area is divided into many chunks
+ * and are maintained.  If chunks are all used up and more is needed,
+ * we cannot simply realloc the block and expand it.  This is because
+ * the realloc may change the addresses and this would not work.  To
+ * provide this expansion requirement, an expanding list of chunk groups
+ * is therefore maintained.  When all the chunks in a group are exhausted,
+ * a new group is created.  When a new chunk group is created, all the
+ * available chunk addresses are added to the stack of free chunks in the
+ * main chunk manager structure.  This causes the chunk manager to
+ * pause and add new chunks to the free chunks stack when the current
+ * set of chunks are depleted.
+ *
+ * A bigger challenge is when 'trimming' the structure is needed.
+ * This is when the user decides that there are a lot of free chunks
+ * around which are not needed and may have to be returned back to
+ * memory.  When this is requested, all the groups are scanned and
+ * for the ones who have all their chunks unused, each chunk is
+ * removed off the free chunks stack and the entire group deleted.
+ * This can be a bit tricky and time consuming and is therefore not
+ * automatically performed but left to the user as to when it needs
+ * to be run.
+ *
+ */
+
 typedef struct chunk_header_s chunk_header_t;
-typedef struct chunk_list_s chunk_list_t;
+typedef struct chunk_group_s chunk_group_t;
 typedef struct chunk_manager_s chunk_manager_t;
 
 struct chunk_header_s {
 
-    chunk_header_t *prev, *next;
+    /*
+     * The chunk group I belong to so that when I am freed up,
+     * I will return to the same group.
+     */
+    chunk_group_t *my_group;
+
+    /*
+     * this is what is returned back to the user and it must
+     * always be 8 bytes aligned, hence the use of long long int.
+     */
     long long int data [0];
 };
 
-struct chunk_list_s {
+/*
+ * Each group has a big memory block which is divided into chunk size
+ * sections.  Addresses of each of these chunks are stored in the main
+ * chunk manager's free_chunks_stack array, which grows by using alloc
+ * & realloc.  When the chunks in a group are all used up, a new group
+ * is created and the new chunks are all added to this stack of free
+ * chunks.
+ */
+struct chunk_group_s {
 
-    chunk_header_t *head, *tail;
-    int n;
+    /* The chunk manager I belong to */
+    chunk_manager_t *my_manager;
+
+    /* big bulk of the memory, all chunks adjacent in one big block */
+    void *chunks_block;
+
+    /* how many free chunks are in this group */
+    int n_grp_free;
+
+    /* next group in list */
+    chunk_group_t *next;
+
 };
 
 struct chunk_manager_s {
 
     MEM_MON_VARIABLES;
     LOCK_VARIABLES;
+
+    /* size of each chunk the user wants and how much it internally takes*/
     int chunk_size;
-    int expansion;
-    int should_not_be_modified;
-    chunk_list_t free_chunks;
+    int actual_chunk_size;
+
+    /* how many chunks per group is needed */
+    int chunks_per_group;
+
+    /* total number of groups so far in the manager */
+    int n_groups;
 
     /*
-     * The 'used_list' is maintained for a very useful purpose.
-     * Whatever data structure the chunks are used for, they can be
-     * traversed easily by simply traversing this list one chunk
-     * at a time.  Since it is much easier to traverse a linked
-     * list, it is very convenient.
+     * Total free chunks in all of the groups of the manager.  If this
+     * is zero, it is time to create another group & add it to the
+     * manager.
      */
-    chunk_list_t used_chunks;
+    int n_cmgr_free;
 
-    /* used for traversing */
-    chunk_header_t *iterator;
+    /* a stack of all the free chunks in the entire manager */
+    chunk_header_t **free_chunks_stack;
+    int free_chunks_stack_index;
+
+    /* a linked list of all the groups */
+    chunk_group_t *groups;
+
 };
 
-int
-chunk_manager_init (chunk_manager_t *cmgr,
-        int make_it_thread_safe,
-        int chunk_size, int expansion,
-        int initial_size, int *actual_chunks_available,
-        mem_monitor_t *parent_mem_monitor);
+/*
+ * Redefine these as per your own requirements
+ */
 
-void *
-chunk_manager_alloc (chunk_manager_t *cmgr);
+#define MIN_CHUNK_SIZE          8
+#define MAX_CHUNK_SIZE          256
+#define MIN_CHUNKS_PER_GROUP    64
+#define MAX_CHUNKS_PER_GROUP    (0x3FFF)
 
-int
-chunk_manager_free (chunk_manager_t *cmgr, void *data);
+extern int
+chunk_manager_init (chunk_manager_t *cmgrp,
+    boolean make_it_thread_safe,
+    int chunk_size, int chunks_per_group,
+    mem_monitor_t *parent_mem_monitor);
 
-void *
-chunk_manager_iterate_start (chunk_manager_t *cmgr);
+extern void *
+chunk_manager_alloc (chunk_manager_t *cmgrp);
 
-void *
-chunk_manager_iterate_next (chunk_manager_t *cmgr);
+extern void
+chunk_manager_free (void *chunk);
 
-void
-chunk_manager_iterate_stop (chunk_manager_t *cmgr);
-
-void
-chunk_manager_trim (chunk_manager_t *cmgr);
-
-void
-chunk_manager_destroy (chunk_manager_t *cmgr);
+extern int
+chunk_manager_trim (chunk_manager_t *cmgrp);
 
 #ifdef __cplusplus
 } // extern C
 #endif
 
-#endif /* __CHUNK_MANAGER_H__ */
+#endif // __CHUNK_MANAGER_H__
 
 
